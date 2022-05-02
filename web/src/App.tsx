b7ae59026590be3955e07791653b7318cfefc3a1
laerthnardelli <laerthnardelli@gmail.com>
2022-05-02 13:28:20 -0300

    standard that will be used in NLW return

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,12 +3,23 @@ interface ButtonProps {
 }
 
 function Button(props: ButtonProps) {
-  return <button className="button">{props.text ?? "Default"}</button>;
+  return (
+    <button
+      className="
+    bg-violet-500 
+      px-4 h-10 
+    text-violet-100
+     hover:bg-violet-700 transaction-colors
+     "
+    >
+      {props.text ?? "Default"}
+    </button>
+  );
 }
 
 function App() {
   return (
-    <div className="gap-button">
+    <div className="flex gap-2">
       <Button text="Enviar" />
       <Button text="OK" />
       <Button />
