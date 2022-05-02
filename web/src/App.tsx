2e61b218a6523649448771b592cf0c1f4cd53727
laerthnardelli <laerthnardelli@gmail.com>
2022-05-02 13:35:14 -0300

    cleaning the app examples to start the project, using a basic structure

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,30 +1,3 @@
-interface ButtonProps {
-  text?: string;
+export function App() {
+  return <h1>Hello Dev</h1>;
 }
-
-function Button(props: ButtonProps) {
-  return (
-    <button
-      className="
-    bg-violet-500 
-      px-4 h-10 
-    text-violet-100
-     hover:bg-violet-700 transaction-colors
-     "
-    >
-      {props.text ?? "Default"}
-    </button>
-  );
-}
-
-function App() {
-  return (
-    <div className="flex gap-2">
-      <Button text="Enviar" />
-      <Button text="OK" />
-      <Button />
-    </div>
-  );
-}
-
-export default App;
