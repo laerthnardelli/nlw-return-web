81efc7b0e39c26a18911e0c02ebee1320db78358
laerthnardelli <laerthnardelli@gmail.com>
2022-05-02 12:07:04 -0300

    example of using TS

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,18 @@
+interface ButtonProps {
+  text: string;
+}
+
+function Button(props: ButtonProps) {
+  return <button>{props.text}</button>;
+}
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <Button text="Enviar" />
+      <Button text="OK" />
+    </div>
+  );
 }
 
 export default App;
