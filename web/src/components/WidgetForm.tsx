077de6be0c99e681dd73c948b13c13680b52e45d
laerthnardelli <laerthnardelli@gmail.com>
2022-05-03 20:56:44 -0300

    added component CloseButton

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,12 +1,15 @@
+import { CloseButton } from "./CloseButton";
+
 export function WidgetForm() {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       <header>
         <span className="text-xl leading-6">Deixe seu feedback</span>
       </header>
+      <CloseButton />
       <p>Hello World</p>
       <footer>
-              Feito com ♥ pela {" "}
+        Feito com ♥ pela{" "}
         <a
           className="underline underline-offset-2"
           href="https://https://www.rocketseat.com.br/"
