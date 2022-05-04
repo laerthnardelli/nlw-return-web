967100c5771343d89840d23860c000be995952ef
laerthnardelli <laerthnardelli@gmail.com>
2022-05-03 21:33:20 -0300

    added styles in button feedback

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -40,7 +40,10 @@ export function WidgetForm() {
         {Object.entries(feedbackTypes).map(([key, value]) => {
           //console.log(key, value);
           return (
-            <button>
+            <button
+              className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+              type="button"
+            >
               <img src={value.image.source} alt={value.image.alt} />
               <span>{value.title}</span>
             </button>
