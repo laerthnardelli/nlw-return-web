5b44c363a22717eaf2a950b60ce6f795d073547d
laerthnardelli <laerthnardelli@gmail.com>
2022-05-03 21:51:55 -0300

    added type "FeedbackType" - to use in onClick

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -3,6 +3,7 @@ import { CloseButton } from "./CloseButton";
 import bugImageUrl from "../assets/bug.svg";
 import ideaImageUrl from "../assets/idea.svg";
 import thoughtImageUrl from "../assets/thought.svg";
+import { useState } from "react";
 
 const feedbackTypes = {
   BUG: {
@@ -28,7 +29,11 @@ const feedbackTypes = {
   },
 };
 
+type FeedbackType = keyof typeof feedbackTypes;
+
 export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       <header>
@@ -36,20 +41,26 @@ export function WidgetForm() {
         <CloseButton />
       </header>
 
-      <div className="flex py-8 gap-2 w-full">
-        {Object.entries(feedbackTypes).map(([key, value]) => {
-          //console.log(key, value);
-          return (
-            <button
-              className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-              type="button"
-            >
-              <img src={value.image.source} alt={value.image.alt} />
-              <span>{value.title}</span>
-            </button>
-          );
-        })}
-      </div>
+      {!feedbackType ? (
+        <div className="flex py-8 gap-2 w-full">
+          {Object.entries(feedbackTypes).map(([key, value]) => {
+            //console.log(key, value);
+            return (
+              <button
+                key={key}
+                className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                type="button"
+                onClick={() => setFeedbackType(key as FeedbackType)}
+              >
+                <img src={value.image.source} alt={value.image.alt} />
+                <span>{value.title}</span>
+              </button>
+            );
+          })}
+        </div>
+      ) : (
+        <p>Hello World</p>
+      )}
 
       <footer>
         Feito com ♥ pela{" "}
