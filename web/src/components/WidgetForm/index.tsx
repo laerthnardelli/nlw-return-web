badaa4303e2f72db415d3d1e4a498928e2445667
laerthnardelli <laerthnardelli@gmail.com>
2022-05-03 22:39:05 -0300

    componentized FeedbackContentStep

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -1,10 +1,9 @@
-import { CloseButton } from "../CloseButton";
-
 import bugImageUrl from "../../assets/bug.svg";
 import ideaImageUrl from "../../assets/idea.svg";
 import thoughtImageUrl from "../../assets/thought.svg";
 import { useState } from "react";
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 
 export const feedbackTypes = {
   BUG: {
@@ -37,15 +36,10 @@ export function WidgetForm() {
 
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      <header>
-        <span className="text-xl leading-6">Deixe seu feedback</span>
-        <CloseButton />
-      </header>
-
       {!feedbackType ? (
         <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
       ) : (
-        <p>Hello World</p>
+        <FeedbackContentStep feedbackType={feedbackType} />
       )}
 
       <footer>
