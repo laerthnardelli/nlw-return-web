b8b66726f2812e75867602bcba49ca6fd8f5c96a
laerthnardelli <laerthnardelli@gmail.com>
2022-05-08 18:24:06 -0300

    added integration with backend

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,7 @@
 import { ArrowLeft } from "phosphor-react";
 import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
+import { api } from "../../../lib/api";
 import { CloseButton } from "../../CloseButton";
 import { ScreenshotButton } from "../ScreenshotButton";
 
@@ -21,9 +22,16 @@ export function FeedbackContentStep({
 
   const [comment, setComment] = useState("");
 
-  function handleSubmitFeedback(event: FormEvent) {
+  async function handleSubmitFeedback(event: FormEvent) {
     event.preventDefault();
-    console.log({ screenshot, comment });
+    //console.log({ screenshot, comment });
+
+    await api.post("/feedbacks", {
+      type: feedbackType,
+      comment,
+      screenshot,
+    });
+
     onFeedbackSent();
   }
 
